b6b173a28b23d83bb786badac9fb94b54a2475ff
SkylaBlue <skylablue1337@hotmail.com>
2013-06-25 13:35:24 -0500

    Create Includes.h

---
+++ Includes.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "Colors.h"
+#include "ClearScreen.h"
+
+#define MAX_MENU_ITEMS 4
+#define NAME 0 
+#define COLOR 1
+#define ITEM 2
+#define MENU 3
+#define DOUBLE_ENDL std::cout << std::endl << std::endl;
