44a0764592899cfa179f6bf0fb7c6e36cd809010
SkylaBlue <skylablue1337@hotmail.com>
2013-06-25 13:32:30 -0500

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,13 @@
+class CMenu
+{
+  private:
+		int iMenuIndex;
+	public:
+		void ReturnToMenu();
+		void ChangeName();
+		void Two();
+		void Three();
+		void KeyboardInput();
+		void VoidEnter(int c);
+		void SetNames();
+};
